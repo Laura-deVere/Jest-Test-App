db8920f4fb2f6c049a91cc2949e25494a44e82a1
Laura-deVere <lauradevh@gmail.com>
2020-12-09 12:53:45 -0500

    start tests for guessedWords

---
+++ src/Congrats.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * @function
+ * @param {object} props
+ * @return {JSX.Element}
+ */
+
+
+const Congrats = (props) => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word</span>
+            </div>
+        );
+    } else {
+        return (
+            <div data-test="component-congrats" />
+        )
+    }
+}
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
+}
+
+export default Congrats;
