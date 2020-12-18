764be7a3f3ea46c21907e9dc5019f4020ccb6ecb
Laura-deVere <lauradevh@gmail.com>
2020-12-18 13:46:14 -0500

    set up for guessedwords tests

--- src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from './';
-
-describe('correctGuess', () => {
-    test('returns an action with type `CORRECET_GUESS', () => {
-        const action = correctGuess();
-        expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-    });
-});
